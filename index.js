a4b4d9b6afbec4c7b81567c92d558d1f0b34435e
CodeInMyPC <thucnguyen3724@gmail.com>
2021-12-06 22:30:05 +0700

    fix footer

--- index.js
+++ index.js
@@ -57,3 +57,12 @@ $(document).ready(function () {
         return false;
     });
 });
+
+// ============================== SHOW SCROLL TOP =============================================
+function scrollTop(){
+  const scrollTop = document.getElementById('back-top-top');
+  //When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top');
+}
+
+window.addEventListener('scroll',scrollTop)
